48b73039aca8cf22998bcc342d1e4cd758b780b6
Viktor Wigmore <v.wigmore@student.tudelft.nl>
2018-11-16 15:58:31 +0100

    updated

--- src/MO.java
+++ src/MO.java
@@ -7,57 +7,118 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class MO extends UnicastRemoteObject implements MO_RMI {
+public class MO implements MO_RMI {
 
 	HashMap<Integer,int[]> buffer;
 	List<Object[]> messageBuffer;
 	
 	int[] timestamp;
 	int id;
+	List<String> hosts;
 	
 	protected MO(int id, int numberProc) throws RemoteException {
 		super();
 		this.id = id;
-		
+	
 		timestamp = new int[numberProc];
 		Arrays.fill(timestamp, 0);
 		messageBuffer = new ArrayList<>();
 		buffer = new HashMap<>();
+		
+		hosts = new ArrayList<>();
+		hosts.add("localhost");
 	}
 	
 	public void sendMessage(Object message, int idReciever) throws Exception {
 		String name = "MO" + idReciever;
-		Registry registry = LocateRegistry.getRegistry("localhost");
-		MO process = (MO) registry.lookup(name);
+		
+		MO_RMI process = null;
+		for (String s : hosts) {
+			Registry registry = LocateRegistry.getRegistry(s);
+			try {
+				process = (MO_RMI) registry.lookup(name);
+				break;
+			} catch (NotBoundException e) {
+
+			}
+		}
+//		
+//		Registry registry = LocateRegistry.getRegistry();
+//		MO_RMI process = (MO_RMI) registry.lookup(name);
+//		
+		
 		timestamp[id] = timestamp[id] + 1;
 		process.recieveMessage(message, buffer, timestamp, id);
 		buffer.put(idReciever, timestamp);
 	}
 
-
-
 	@Override
 	public void recieveMessage(Object m, HashMap<Integer, int[]> b, int[] t, int idSender)
 			throws RemoteException {
+//		System.out.println("recieved a message");	
+//		
+//		System.out.print("timestamp: ");
+//		for (int j=0; j<timestamp.length; j++) {
+//			System.out.print(timestamp[j]+" ");
+//		}
+//		System.out.println();
+		
+//		if (b.containsKey(id)) {
+//			System.out.print("timestamp from message: ");
+//			for (int j=0; j<b.get(id).length; j++) {
+//				System.out.print(b.get(id)[j]+" ");
+//			}
+//		}
+//		System.out.println();
+		
+		
+		
+		if ( !(b.containsKey(id) && checkDeliver(timestamp, b.get(id)))) {
+			this.deliver(m, b, t);
 			
-		if ( !b.containsKey(id) || checkDeliver(timestamp, b.get(id))) {
-			this.deliver(m, b);
-			
-			for (Object[] i : messageBuffer) {
-				if (checkDeliver(timestamp, (int[]) i[2])) {
-					deliver(i[0], (HashMap<Integer, int[]>) i[3]);
+			boolean loop = true;
+			while (loop) {
+				loop = false;
+				for (Object[] i : messageBuffer) {
+//					
+					
+					if (checkDeliver(timestamp, (int[]) i[2])) {
+						deliver(i[0], (HashMap<Integer, int[]>) i[3], (int[]) i[2]);
+						loop = true;
+					}
 				}
 			}
+			
+			
 		} else {
 			// add to buffer
 			messageBuffer.add(new Object[] {m, idSender, t, b});
 		}
 		
-		timestamp[id] = timestamp[id] +1;
+//		timestamp[id] = timestamp[id] +1;
 	}
 	
-	private void deliver(Object message, HashMap<Integer, int[]> b) {
+	private void deliver(Object message, HashMap<Integer, int[]> b, int[] t) {
 		System.out.println(message.toString());
+		
+//		System.out.print("t: ");
+//		for (int j=0; j<t.length; j++) {
+//			System.out.print(t[j]+" ");
+//		}
+//		System.out.println();
+		
+		for (int i=0; i<timestamp.length; i++) {
+			if (timestamp[i] < t[i]) {
+				timestamp[i] = t[i];
+			}
+		}
+		
+//		System.out.print("timestamp: ");
+//		for (int j=0; j<timestamp.length; j++) {
+//			System.out.print(timestamp[j]+" ");
+//		}
+//		System.out.println();
+		
 		for (Integer i : b.keySet()) {
 			if (buffer.containsKey(i)) {
 				int[] temp = buffer.remove(i);
@@ -69,19 +130,24 @@ public class MO extends UnicastRemoteObject implements MO_RMI {
 	}
 	
 	private int[] compareArray(int[] x, int[] y) {
-		for (int i : x) {
-			if (x[i] < y[i]) {
-				return y;
+		if (y != null) {
+			for (int i=0; i<x.length; i++) {
+				if (x[i] < y[i]) {
+					return y;
+				}
 			}
 		}
 		return x;
 	}
 	
 	
-	private boolean checkDeliver(int[] x, int[] y) {
-		for (int i : x) {
-			if (x[i] < y[i]) {
-				return false;
+	private boolean checkDeliver(int[] x, int[] y) {		
+
+		if (y != null) {
+			for (int i=0; i<x.length; i++) {
+				if (x[i] < y[i]) {
+					return false;
+				}
 			}
 		}
 		return true;
@@ -89,9 +155,34 @@ public class MO extends UnicastRemoteObject implements MO_RMI {
 
 	public static void main(String args[]) {
 		try {
-			java.rmi.registry.LocateRegistry.createRegistry(1099);
-		} catch (RemoteException e) {
+			// args[0] = id, args[1] = numberProc
+			MO obj = new MO(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+			MO_RMI stub = (MO_RMI)	UnicastRemoteObject.exportObject(obj, 0);
+	        Registry registry = LocateRegistry.getRegistry(); 
+	        registry.bind("MO"+args[0], stub);  
+	        
+	        Thread.sleep(5000);
+			System.out.println("Server ready");
+			
+			// from here send messages
+			for (int i=0; i<10; i++) {
+				Thread.sleep(Math.round(Math.random()*3000));
+				
+				
+				int idrec = (int) Math.floor(Math.random()*3);
+				while (idrec==Integer.parseInt(args[0])) {
+					idrec = (int) Math.floor(Math.random()*3);
+				}
+				
+				obj.sendMessage("This is a message from id:" + args[0] + " for id:"+idrec + " at time:"+System.currentTimeMillis(), idrec);
+
+			}
+			System.out.println("Finished sending messages");
+			System.out.println(obj.messageBuffer.toString());
+			
+		} catch (Exception e) {
 			e.printStackTrace();
+
 		}
 	}
 }
