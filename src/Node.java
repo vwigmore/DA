d0cecf98edeced03bc87ce257607a4a6d9387882
Viktor Wigmore <v.wigmore@student.tudelft.nl>
2018-12-06 11:51:28 +0100

    impl

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,83 @@
+import java.rmi.RemoteException;
+import java.util.HashMap;
 
 public class Node implements Node_RMI {
 
+	private int id;
+	private HashMap<Integer, Double> edgesWeights;
+	private HashMap<Integer,String> SE;
+	private String SN;
+	private String FN;
+	private int LN;
+	
+	private int in_branch = -1;
+	private int test_edge = -1;
+	private int best_edge = -1;;
+	private int best_weight = Integer.MAX_VALUE;
+	private int find_count = 0;
+	
+	
+	public Node(int id, HashMap<Integer, Double> edges) {
+		this.id = id;
+		edgesWeights = edges;
+		
+		for (Integer i : edges.keySet()) {
+			SE.put(i, "?_in_MST");
+		}
+		SN = "sleeping";
+	}
+
+
+	@Override
+	public void connect(int idSender, int L) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void initiate(int idSender, int L, String F, String S) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void test(int idSender, int L, String F) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void reject(int idSender) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void accept(int idSender) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void report(int idSender, int W) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void change_root() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
 }
--- src/Node_RMI.java
+++ src/Node_RMI.java
@@ -1,5 +1,23 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Node_RMI extends Remote {
 
+	public void connect(int idSender ,int L) throws RemoteException;
+	
+	public void initiate(int idSender, int L, String F, String S) throws RemoteException;
+	
+	public void test(int idSender, int L, String F) throws RemoteException;
+	
+	public void reject(int idSender) throws RemoteException;
+	
+	public void accept(int idSender) throws RemoteException;
+	
+	public void report(int idSender, int W) throws RemoteException;
+	
+	public void change_root() throws RemoteException;
+
+	
+	
+	
 }
